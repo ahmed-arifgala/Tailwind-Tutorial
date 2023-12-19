5c609c456c476f158319487abf71b3b18a649c8c
ahmed-arifgala <126764357+ahmed-arifgala@users.noreply.github.com>
2023-12-19 07:38:10 +0500

    Tailored Config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+//tailwinfd uses this file to convert those that you have wriiten in html to convert into css
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      mammoth: '8rem'
+    },
+    extend: {
+      colors:{
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      }
+    },
   },
   plugins: [],
 }
